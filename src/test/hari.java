5fe8511c25fb351586672faac4188412bd41f0c2
krishna2531 <harikrishnatkpd@gmail.com>
2023-06-09 00:22:22 +0530

    git project testing

---
+++ src/test/hari.java
@@ -0,0 +1,14 @@
+package test;
+
+public class hari {
+
+	
+	public void m1() {
+		System.out.println("i am hari");
+	}
+	public static void main(String[] args) {
+		 
+		hari k=new hari();
+		k.m1();
+	}
+}
